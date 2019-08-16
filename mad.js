ad84f0ca33116afe5c33978fec78c1948be795b1
dinaftensmad <54064091+dinaftensmad@users.noreply.github.com>
2019-08-16 08:50:06 +0200

    Add files via upload

--- mad.js
+++ mad.js
@@ -1,8 +1,9 @@
-function randomMad() {
+		function randomMad() {
 	//var mad = ['Cheese Burger', 'Mac N\'cheese', 'Frikadeller', 'Nachos', ];
-	//var desert = ['Regnbue is', 'Chokolademouse', 'Romkugler',]
-	var desert = ['Regnebueis.jpg', 'Romkugler.jpg'];
-	var mad = ['Frikadeller.jpg', 'Mac n\' Cheese.jpg', 'Cheeseburger.jpg'];
+	//var desert = ['Regnbue is', 'Chokolademouse', 'Romkugler'];
+	var desert = ['Regnbueis.jpg', 'Romkugler.jpg'];
+	var mad = ['Frikadeller.jpg', 'Mac n\' Cheese.jpg', 'Cheeseburger.jpg' 'Nachos.jpg'];
+	//var drik = [''];
 	var madIndex = Math.floor(mad.length * Math.random());
 	var desertIndex = Math.floor(desert.length * Math.random());
 	var linkDesertOpskrifter = [
@@ -22,4 +23,20 @@ function randomMad() {
 	document.getElementById("desertTxt").innerHTML = 'Desert: ' + desert[desertIndex].substring(0, desert[desertIndex].length - 4);
 	document.getElementById("madLinks").href = linkMadOpskrifter[madIndex];
 	document.getElementById("deLinks").href = linkDesertOpskrifter[desertIndex];
+	
+}
+
+function randomOpskrifter() {
+	var opskriftLinks = [
+	'https://www.alt.dk/mad/opskrift/klassiske-frikadeller', 
+	'https://vielskermad.dk/opskrift/macaroni-cheese/', 
+	'https://www.dr.dk/mad/opskrift/cheeseburger-nem-og-hurtig-udgave',
+	'https://www.alt.dk/mad/opskrift/regnbueis', 
+	'http://www.mums-opskrifter.dk/romkugler/'
+	];
+	
+	
+	//var linksIndex = Math.floor(linksIndex.length * Math.random());
+	
+	//document.getElementById("lykkebtn").href = opskriftLinks;
 }
